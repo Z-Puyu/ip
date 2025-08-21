127b9e1ea6c5cfe1fa2ed7bab9405a6db8bbe679
Z-Puyu <zpyzhangpuyu@gamil.com>
2025-08-22 03:57:30 +0800

    Add custom enum string representations

--- src/main/java/inputs/InputAction.java
+++ src/main/java/inputs/InputAction.java
@@ -3,11 +3,46 @@ package inputs;
 public enum InputAction {
     Quit,
     Undefined,
-    DeleteTask,
-    CreateTodo,
-    CreateDeadline,
-    CreateEvent,
-    DenumerateTasks,
-    MarkTask,
-    UnmarkTask
+    DeleteTask {
+        @Override
+        public String toString() {
+            return "Delete Task";
+        }
+    },
+    CreateTodo {
+        @Override
+        public String toString() {
+            return "Create Todo";
+        }
+    },
+    CreateDeadline {
+        @Override
+        public String toString() {
+            return "Create Task with Deadline";
+        }
+    },
+    CreateEvent {
+        @Override
+        public String toString() {
+            return "Create Event";
+        }
+    },
+    DenumerateTasks {
+        @Override
+        public String toString() {
+            return "Denumerate Tasks";
+        }
+    },
+    MarkTask {
+        @Override
+        public String toString() {
+            return "Mark Task";
+        }
+    },
+    UnmarkTask {
+        @Override
+        public String toString() {
+            return "Unmark Task";
+        }
+    }
 }
